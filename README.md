d92275d0bda8baa31b81a41830eee25ce01d4d02
Salvatore Laisa <salvatore.laisa@musement.com>
2021-08-02 20:25:42 +0200

    v 1.0

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # imarcuz
+> Remake of my first ever iPhone app, now built with Flutter.
 
-A new Flutter project.
+My first iPhone app ever was this simple parody random-phrase-shuffler app designed around some weird statements from a friend of mine.
 
-## Getting Started
+Now I've rebuilt it from scracth, bringing back some old assets I've found in my Dropbox folder and rebuilding it using Google's [Flutter](https://flutter.dev/) toolkit. 
 
-This project is a starting point for a Flutter application.
+This was a simple app back then and it is still so right now... this is basically a customization of the starting template of Flutter projects
 
-A few resources to get you started if this is your first Flutter project:
+## Platform
+Right now I'm mainly targeting the [Android](https://www.android.com/) platform since I'm not going to release this into any app store, but just sending the `.apk` build to friends and colleagues.
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+But since this project is written on top of Flutter, it can eventually be also be build for [iOS](https://www.apple.com/it/ios).
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## License
+Released under the [MIT license](LICENSE).
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 var list = [
   "Mangi gratis se fai il becchino!",
@@ -48,44 +47,50 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final _buttonStyle = ElevatedButton.styleFrom(
+        primary: Colors.black45,
+        textStyle: const TextStyle(fontSize: 24),
+        padding: EdgeInsets.symmetric(horizontal: 70, vertical: 20));
+
     return Scaffold(
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("images/background.jpg"),
+              fit: BoxFit.contain,
+              alignment: Alignment.topCenter),
+        ),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
-            Text(
-              '$_current',
-              style: Theme.of(context).textTheme.headline4,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(
+                    child: ElevatedButton(
+                  onPressed: _shuffleItem,
+                  child: Text(
+                    "$_current",
+                    textAlign: TextAlign.center,
+                  ),
+                  style: _buttonStyle,
+                ))
+              ],
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    margin: EdgeInsets.only(top: 50.0, bottom: 80),
+                    child: ElevatedButton(
+                      onPressed: _shuffleItem,
+                      child: Text("Un'altra frase!"),
+                      style: _buttonStyle,
+                    ))
+              ],
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _shuffleItem,
-        tooltip: 'Cambia frase',
-        child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
